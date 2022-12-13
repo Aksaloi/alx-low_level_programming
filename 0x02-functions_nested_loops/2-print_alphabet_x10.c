23df2d9cf031e58df23169b9d0b34a73c7d3051f
Aksaloi <akezasaloi@gmail.com>
2022-12-13 10:16:36 -0800

    task 2.10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char le;
+	int ten;
+
+	for (ten = 0; ten < 9; ten++)
+	{
+		for (le = 'a'; le <= 'z'; le++)
+			_putchar(le);
+		_putchar('\n');
+	}
+}
