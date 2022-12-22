85cecf5356feb2826adae2396d43229a6bb68dcf
Aksaloi <akezasaloi@gmail.com>
2022-12-22 14:24:54 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+##include "main.h"
+/**
+ * strcat - concatenates strings
+ * @src: the source string
+ * @dest: pointer to the string
+ * Return: Apointer to the destination string @dest.
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
