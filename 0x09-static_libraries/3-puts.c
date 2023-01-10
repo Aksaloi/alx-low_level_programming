3581ca06d3f74e19ddc37a8c7d4b77a17163eb0f
Aksaloi <akezasaloi@gmail.com>
2023-01-10 14:31:08 -0800

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,19 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * _puts - writes the string str to stdout
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+	str[] = "\"Programming is like building a multilingual puzzle";
+}
 /**
  * main - Entry point
- * _puts: returns the sentencence inputed
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(str);
 	return (0);
 }
